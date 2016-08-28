931fb187798b0c784a5eb23dfdc3f4da9319431e
Lemonlemons <moeandrew@hotmail.com>
2016-08-27 21:01:23 -0500

    more asset changing 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Add additional assets to the asset load path
+  config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
